dc9cc2aa9319fa09ae0177f1eec3d433d1191995
Maks6666 <maxim.kucher2005@gmail.com>
2024-04-02 20:30:29 +0300

    1st task

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
+class Calculator:
+    def add(self, x, y):
+        return x + y
+    def subtract(self, x, y):
+        return x - y
+    def multiply(self, x, y):
+        return x * y
+    def divide(self, x, y):
+        if y != 0:
+            return x / y
+        else:
+            raise ZeroDivisionError("Zero division error")
+    def __call__(self, action, x, y):
+        if action == "add":
+            return self.add(x, y)
+        elif action == "subtract":
+            return self.subtract(x, y)
+        elif action == "multiply":
+            return self.multiply(x, y)
+        elif action == "divide":
+            return self.divide(x, y)
+        else:
+            print("Wrong action.")
 
-
-
+calculator = Calculator()
+print(calculator("add", 2, 4))
 
 
 
